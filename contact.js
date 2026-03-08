d0770b1caba6fc839386f829981d659e4a844da6
Mbachu Emmanuel <emmanuelebubembachu@gmail.com>
2026-03-08 16:39:00 +0000

    cleard folder

--- contact.js
+++
@@ -1,7 +0,0 @@
-// contact.js
-
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for reaching out! I'll get back to you soon.");
-    this.reset();
-});
